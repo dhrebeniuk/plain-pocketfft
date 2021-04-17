0e0d66e8b795707c0dac4df2329df6d9dd343901
Dmytro Hrebeniuk <dmytrohrebeniuk@gmail.com>
2021-04-17 21:55:07 +0300

    Init plain-pocketfft project

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.1
+
+import PackageDescription
+
+let package = Package(
+    name: "PlainPocketFFT",
+    products: [
+        .library(
+            name: "PlainPocketFFT",
+            targets: ["PlainPocketFFT"]),
+    ],
+    dependencies: [
+    ],
+    targets: [
+        .target(
+            name: "PlainPocketFFT",
+            dependencies: []),
+        
+    ]
+)
